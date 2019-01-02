0b0b133be16127470abf96d9ce9f95cf262e321e
Ben Quartermaine <ben.quartermaine3@gmail.com>
2019-01-02 22:15:51 +0800

    reducer and actions file structure updted

---
+++ src/reducers/flats_reducer.js
@@ -0,0 +1,19 @@
+// Receives an action that that changes the current state by performing an action
+// Reducers goal is to compute and return the new state
+
+export default function(state, action) {
+  // Incase there is no flats in the array (prevents throwing an error)
+  if (state === undefined) {
+    return [];
+  }
+
+  // TODO Handle actions
+  switch(action.type) {
+    case 'SET_FLATS': 
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+
